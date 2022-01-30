6abcd4ab41d2b436c8d99c44e47dff8bafafb758
hinomi <nicolas.treiber@gmail.com>
2022-01-30 12:13:42 +0000

    Added teleporter component

---
+++ Assets/Scripts/Levels/Teleporter.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Levels {
+	public class Teleporter : MonoBehaviour {
+		[SerializeField] private GameObject teleportedObject;
+		[SerializeField] private Transform target;
+
+		private void OnStand(GameObject obj) {
+			if (obj == teleportedObject) {
+				obj.transform.position = target.position;
+			}
+		}
+	}
+}
